29c4dffd9e32c52c5a4f9845437bca178a9014b9
asterrline <bzszly1o@gmail.com>
2026-06-18 11:54:56 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,5 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-  root: resolve(__dirname, 'src'),
-  base: './',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
-  server: {
-    open: true,
-    port: 5173,
-  }
+  base: '/mesto-/'
 });
